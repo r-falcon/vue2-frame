ba73bcfec50f2d965056ff415c10a549a1fb05a6
r-falcon <rfalcon@163.com>
2021-12-06 17:30:01 +0800

    -详情展示封装

---
+++ src/components/Description/Description.vue
@@ -0,0 +1,23 @@
+<script type="text/jsx">
+import styles from './Description.module.scss';
+
+export default {
+  name: 'Description',
+  functional: true,
+  render: function(h, context) {
+    const { label, value, width = 'auto' } = context.props;
+    return (
+      <div class={styles.description_content} style={{ width: width }}>
+        {
+          label ? <span class={styles.description_content_label}>{label}:</span> : null
+        }
+        <span class={styles.description_content_value}>
+          {
+            (value || context.children) || '暂无信息'
+          }
+        </span>
+      </div>
+    );
+  }
+};
+</script>
--- src/views/options/page-demo/PageDetail.vue
+++ src/views/options/page-demo/PageDetail.vue
@@ -1,20 +1,35 @@
 <template>
- <Subpage content="详情页" @back="goBack">
-   这是页面详情1111
- </Subpage>
+  <Subpage content="详情页" @back="goBack">
+    <div class="show">
+      <Description width="50%" label="表演类型">迷惑行为</Description>
+      <Description width="50%" label="获奖类型">年度最佳</Description>
+      <Description width="50%" label="主要嘉宾">全体员工</Description>
+      <Description width="200%" label="获奖感言">暂时没有111111111111111111111111111111111111111111111111暂时没有111111111111111111111111111111111111111111111111</Description>
+    </div>
+  </Subpage>
 </template>
 
 <script>
-import {Subpage} from '@/components/Subpage'
+import { Subpage } from '@/components/Subpage'
+import { Description } from '@/components/Description'
 
 export default {
-  components:{
-    Subpage
+  components: {
+    Subpage,
+    Description
   },
   methods: {
-    goBack(){
+    goBack () {
       this.$router.go(-1)
     }
   },
 }
 </script>
+
+<style scoped>
+.show {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
