16a25a119ab75832d9b8cdaba81474c57ae6a28f
r-falcon <rfalcon@163.com>
2021-12-08 17:21:11 +0800

    -修复字典管理bug

--- src/views/options/dict-demo/DictPanel.vue
+++ src/views/options/dict-demo/DictPanel.vue
@@ -3,47 +3,57 @@
     数据字典模型:
     <p>statusOptions:</p>
     <div v-for="item in statusOptions" :key="item.value">
-      {{item.value}}-{{item.label}}
+      {{ item.value }}-{{ item.label }}
     </div>
-    <treeselect 
+    <treeselect
       disableBranchNodes
       :multiple="true"
       :show-count="true"
       :options="levelOptions"
       placeholder="请选择"
-      style="width:50%;"
+      style="width: 50%"
     />
+    <div v-for="item in roleOptions" :key="item.value">
+      {{ item.value }}-{{ item.label }}
+    </div>
   </div>
 </template>
 
 <script>
-import {mapState,mapMutations} from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 import Treeselect from '@riophae/vue-treeselect';
 import '@riophae/vue-treeselect/dist/vue-treeselect.css';
-import {rightData} from './service'
+import { rightData } from './service'
 
 export default {
-  components:{
+  components: {
     Treeselect
   },
-  computed:{
+  computed: {
     ...mapState({
-      statusOptions:state => state.dict.statusOptions,
-      levelOptions:state => state.dict.levelOptions
-    }),
-    // 1.第一步的导入
-    ...mapMutations({
-      setRoleOptions:'dict/setRoleOptions'
+      statusOptions: state => state.dict.statusOptions,
+      levelOptions: state => state.dict.levelOptions,
+      roleOptions: state => state.dict.roleOptions
     })
   },
-  mounted(){
+  mounted () {
     this.initData()
   },
-  methods:{
-    initData(){
+  methods: {
+    // 1.第一步的导入
+    ...mapMutations({
+      setRoleOptions: 'dict/setRoleOptions'
+    }),
+    initData () {
       rightData().then(res => {
         // 第二步的设置
-        this.setRoleOptions(res.data.map(item => ({label:item.authName,value:item.id})))
+        const result = []
+        res.data.map(item => {
+          if (item.level === '0') {
+            result.push({ label: item.authName, value: item.id })
+          }
+        })
+        this.setRoleOptions(result)
       })
     }
   }
