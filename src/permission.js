2df1d34afeea1f39a3dcf33303973b5428ba85ec
r-falcon <rfalcon@163.com>
2021-12-08 16:27:20 +0800

    -单点登录调整

--- src/permission.js
+++ src/permission.js
@@ -51,74 +51,66 @@ router.beforeEach((to, from, next) => {
   } else {
     // 没有token，单点登录，前端拦截处理
     console.log("to", to.query);
+    console.log(window.location);
+    console.log(window.location.pathname);
     if (
       to.query &&
       to.query.access_token &&
       to.query.privateKey &&
       to.query.publicKey
     ) {
-      //拦截到url中所需要的参数并存储到cookie中,然后跳转到启动页
+      // ?access_token=123&privateKey=456&publicKey=789
+      console.log("sort111");
       setToken(to.query.access_token);
       setPrivateKey(to.query.privateKey);
       setPublicKey(to.query.publicKey);
       store.commit("SET_TOKEN", to.query.access_token);
       next({
-        path: "/launchpad",
+        path: "/",
       });
-    } else if (
-      window.location.pathname !== "/" &&
-      !to.query.ticket &&
-      window.location.pathname !== "login"
-    ) {
-      //如果地址中输入了其他东西，并且ticket为空的时候，直接跳转到一个链接地址
-      const url = `http://12.254.33.12/portal/demo.login?service=${window.location.href}`;
-      window.location.href = url;
-    } else if (to.query.ticket) {
+    } else if (to.query && to.query.ticket) {
+      // ?ticket=abc
       const url = window.location.origin + window.location.pathname;
-      loginTicket(to.query.ticket, url)
-        .then((res) => {
-          setToken(res.data.access_token);
-          setPrivateKey(res.data.privateKey);
-          setPublicKey(res.data.publicKey);
-          store.commit("SET_TOKEN", res.data.access_token);
-          store
-            .dispatch("GetInfo")
-            .then((res) => {
-              // 拉取user_info
-              const roles = res.roles;
-              store
-                .dispatch("GenerateRoutes", {
-                  roles,
-                })
-                .then((accessRoutes) => {
-                  // 测试 默认静态页面
-                  // store.dispatch('permission/generateRoutes', { roles }).then(accessRoutes => {
-                  // 根据roles权限生成可访问的路由表
-                  router.addRoutes(accessRoutes); // 动态添加可访问路由表
-                  next({
-                    path: "/launchpad",
-                    replace: true,
-                  }); // hack方法 确保addRoutes已完成
-                });
-            })
-            .catch((err) => {
-              store.dispatch("FedLogOut").then(() => {
-                Message.error(err);
-                next({
-                  path: "/launchpad",
-                });
+      loginTicket(to.query.ticket, url).then((res) => {
+        setToken(res.data.access_token);
+        setPrivateKey(res.data.privateKey);
+        setPublicKey(res.data.publicKey);
+        store.commit("SET_TOKEN", res.data.access_token);
+        // 判断当前用户是否已拉取完user_info信息
+        store
+          .dispatch("GetInfo")
+          .then(() => {
+            store.dispatch("GenerateRoutes").then((accessRoutes) => {
+              // 根据roles权限生成可访问的路由表
+              router.addRoutes(accessRoutes); // 动态添加可访问路由表
+              next({
+                ...to,
+                replace: true,
+              }); // hack方法 确保addRoutes已完成
+            });
+          })
+          .catch((err) => {
+            store.dispatch("LogOut").then(() => {
+              Message.error(err);
+              next({
+                path: "/",
               });
             });
-        })
-        .catch((err) => console.log(err));
-    } else if (
-      !(to.query.access_token || to.query.privateKey || to.query.publicKey) &&
-      whiteList.indexOf(to.path) !== -1
-    ) {
-      next();
+          });
+      });
     } else {
-      next("/login");
-      NProgress.done();
+      if (whiteList.indexOf(to.path) !== -1) {
+        next();
+      } else if (
+        window.location.pathname !== "/" &&
+        window.location.pathname !== "/login"
+      ) {
+        const url = `http://18.254.33.12/honesty/ssh.login?service=${window.location.href}`;
+        window.location.href = url;
+      } else {
+        next("/login");
+        NProgress.done();
+      }
     }
 
     // 没有token
