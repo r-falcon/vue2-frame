d6c67a39d960f7e44cfc5d3f1af160a334897b1b
r-falcon <rfalcon@163.com>
2022-07-19 09:02:10 +0800

    -update 表格多行拖拽

---
+++ src/views/options/drag-table/demo1.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="draggable" style="padding: 20px">
+    <el-table row-key="id" :data="tableData" style="width: 100%" border>
+      <el-table-column
+        v-for="(item, index) in oldList"
+        :key="`col_${index}`"
+        :prop="newList[index].prop"
+        :label="item.label"
+        align="center"
+      >
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+import Sortable from "sortablejs";
+
+export default {
+  mounted() {
+    this.oldList = JSON.parse(JSON.stringify(this.tableConfig.tableItems));
+    this.newList = JSON.parse(JSON.stringify(this.tableConfig.tableItems));
+    this.columnDrop();
+    this.rowDrop();
+  },
+  data() {
+    return {
+      oldList: [],
+      newList: [],
+      tableData: [
+        {
+          id: 1,
+          name: "李一",
+          gender: "男",
+          age: 30,
+          job: "会计",
+        },
+        {
+          id: 2,
+          name: "王二",
+          gender: "未知",
+          age: 18,
+          job: "无业游民",
+        },
+        {
+          id: 3,
+          name: "张三",
+          gender: "男",
+          age: 50,
+          job: "老板",
+        },
+      ],
+      tableConfig: {
+        tableItems: [
+          {
+            label: "序号",
+            prop: "id",
+          },
+          {
+            label: "姓名",
+            prop: "name",
+          },
+          {
+            label: "性别",
+            prop: "gender",
+          },
+          {
+            label: "年龄",
+            prop: "age",
+          },
+          {
+            label: "工作",
+            prop: "job",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    // 行拖拽
+    rowDrop() {
+      // 此时找到的元素是要拖拽元素的父容器
+      const tbody = document.querySelector(".draggable .el-table__body-wrapper tbody");
+      const _this = this;
+      Sortable.create(tbody, {
+        //  指定父元素下可被拖拽的子元素
+        draggable: ".draggable .el-table__row",
+        onEnd({ newIndex, oldIndex }) {
+          const currRow = _this.tableData.splice(oldIndex, 1)[0];
+          _this.tableData.splice(newIndex, 0, currRow);
+        },
+      });
+    },
+    // 列拖拽
+    columnDrop() {
+      const wrapperTr = document.querySelector(".draggable .el-table__header-wrapper tr");
+      this.sortable = Sortable.create(wrapperTr, {
+        animation: 180,
+        delay: 0,
+        onEnd: (evt) => {
+          const oldItem = this.newList[evt.oldIndex];
+          this.newList.splice(evt.oldIndex, 1);
+          this.newList.splice(evt.newIndex, 0, oldItem);
+        },
+      });
+    },
+  },
+};
+</script>
+<style scoped></style>
---
+++ src/views/options/drag-table/demo2.vue
@@ -0,0 +1,221 @@
+<template>
+  <div style="padding: 12px">
+    <div>
+      <el-button @click="moveUp" type="primary" size="small">上移</el-button>
+      <el-button @click="moveDown" type="primary" size="small">下移</el-button>
+    </div>
+    <el-table-draggable>
+      <el-table
+        row-key="id"
+        ref="table2"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column label="ID" width="60">
+          <template slot-scope="scope">{{ scope.row.id }}</template>
+        </el-table-column>
+        <el-table-column label="日期" width="120">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+        <el-table-column prop="address" label="地址" show-overflow-tooltip>
+        </el-table-column>
+      </el-table>
+    </el-table-draggable>
+  </div>
+</template>
+
+<script>
+import ElTableDraggable from "el-table-draggable";
+export default {
+  name: "Table2",
+  components: {
+    ElTableDraggable,
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 2,
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 3,
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 4,
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 5,
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 6,
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 7,
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+      multipleSelection: [],
+    };
+  },
+  methods: {
+    moveUp() {
+      if (this.multipleSelection.length === 0) {
+        this.$message({
+          message: "请至少选择一条数据进行操作。",
+          type: "warning",
+        });
+        return false;
+      }
+
+      if (this.tableData.length === this.multipleSelection.length) {
+        this.$message({
+          message: "不能选择全部数据上移。",
+          type: "warning",
+        });
+        return false;
+      }
+
+      // confirm slotIndex
+      let slotIndex = -1;
+      this.tableData.forEach((item, index) => {
+        this.multipleSelection.forEach((v, k) => {
+          if (item.id === v.id) {
+            if (slotIndex === -1) {
+              slotIndex = index;
+            } else {
+              if (slotIndex > index) {
+                slotIndex = index;
+              }
+            }
+          }
+        });
+      });
+
+      // delete selectdata
+      this.multipleSelection.forEach((item, index) => {
+        this.tableData.forEach((v, k) => {
+          if (item.id === v.id) {
+            this.tableData.splice(k, 1);
+          }
+        });
+      });
+
+      if (slotIndex > 0) {
+        slotIndex = slotIndex - 1;
+      } else {
+        slotIndex = 0;
+      }
+      this.tableData.splice(slotIndex, 0, this.multipleSelection);
+      this.tableData = this.tableData.flat();
+
+      const selectionTableData = this.multipleSelection;
+      setTimeout(async () => {
+        await this.$nextTick();
+        selectionTableData.forEach((row) => {
+          this.$refs.table2.toggleRowSelection(row);
+        });
+      }, 10);
+    },
+
+    moveDown() {
+      if (this.multipleSelection.length === 0) {
+        this.$message({
+          message: "请至少选择一条数据进行操作。",
+          type: "warning",
+        });
+        return false;
+      }
+
+      if (this.tableData.length === this.multipleSelection.length) {
+        this.$message({
+          message: "不能选择全部数据下移。",
+          type: "warning",
+        });
+        return false;
+      }
+
+      // confirm slotIndex
+      let slotIndex = -1;
+      this.tableData.forEach((item, index) => {
+        if (this.multipleSelection.length > 1) {
+          this.multipleSelection.forEach((v, k) => {
+            if (item.id === v.id) {
+              if (slotIndex === -1) {
+                slotIndex = index;
+              } else {
+                if (slotIndex < index) {
+                  slotIndex = index;
+                }
+              }
+            }
+          });
+        } else {
+          this.multipleSelection.forEach((v, k) => {
+            if (item.id === v.id) {
+              slotIndex = index + 1;
+            }
+          });
+        }
+      });
+
+      // delete selectedata
+      this.multipleSelection.forEach((item, index) => {
+        this.tableData.forEach((v, k) => {
+          if (item.id === v.id) {
+            this.tableData.splice(k, 1);
+          }
+        });
+      });
+
+      if (slotIndex > 0) {
+        // slotIndex = slotIndex
+      } else {
+        slotIndex = 1;
+      }
+      this.tableData.splice(slotIndex, 0, this.multipleSelection);
+      this.tableData = this.tableData.flat();
+
+      const selectionTableData = this.multipleSelection;
+      setTimeout(async () => {
+        await this.$nextTick();
+        selectionTableData.forEach((row) => {
+          this.$refs.table2.toggleRowSelection(row);
+        });
+      }, 10);
+    },
+
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+      console.log(this.multipleSelection);
+    },
+  },
+};
+</script>
+<style></style>
